f927ff0b833c1373b409b898a6f96f42e7d7b65b
ECrystal123 <zhz20121211@126.com>
2025-04-20 09:57:52 +0800

    update

--- script/folders.js
+++
@@ -1,9 +0,0 @@
-const folders = [
-{ name: 'music', path: 'music/music.html', size: '8.0K' },
-{ name: 'style', path: 'style/style.html', size: '16K' },
-{ name: 'js', path: 'js/js.html', size: '16K' },
-];
-{ name: 'mirror', path: 'mirror/mirror.html', size: '48K' },
-];
-{ name: 'audio', path: 'audio/audio.html', size: '3.3M' },
-];
